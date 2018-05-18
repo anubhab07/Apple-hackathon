d5bfa0a8b135e16da47eddc92470e398667c91ea
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2018-05-19 02:19:14 +0530

    dist path updated

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 
 const app = express();
 
+app.listen(process.env.PORT || 8080);
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/apple-hackathon/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 // app.get('/*', function(req,res) {
     
@@ -21,4 +22,6 @@ res.sendFile(path.join(__dirname+'/dist/apple-hackathon/index.html'));
 // });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+
+console.log("app started")
