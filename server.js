0255820aeb1fa04257babd6b58b8994d8fae0d64
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2018-05-18 13:34:33 +0530

    Edit server js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Install express 
-serverconst express = require('express');const path = require('path'); 
+const express = require('express');const path = require('path'); 
 const app = express();
 //Serve only the static files form 
  directoryapp.use(express.static(__dirname + '/dist'));
