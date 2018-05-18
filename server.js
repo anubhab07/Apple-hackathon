79a97280dbfa66271f1a98911cd1f4927991b63e
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2018-05-19 01:51:41 +0530

    server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/apple-hackathon/index.html'));
 });
 // app.get('/*', function(req,res) {
     
